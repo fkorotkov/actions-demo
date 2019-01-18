06a6cbfa63007f43af7d96bb49e76ed9863a94aa
Fedor Korotkov <fedor.korotkov@gmail.com>
2019-01-18 16:55:02 -0500

    Actual rebase

--- rebase/entrypoint.sh
+++ rebase/entrypoint.sh
@@ -26,13 +26,29 @@ AUTH_HEADER="Authorization: token $GITHUB_TOKEN"
 pr_resp=$(curl -XGET -sSL -H "${AUTH_HEADER}" -H "${API_HEADER}" \
           "${URI}/repos/$REPO_FULLNAME/pulls/$PR_NUMBER")
 
-echo "API response $pr_resp"
-
-BASE_BRANCH=$(echo "$resp" | jq -r .base.ref)
+BASE_REPO=$(echo "$pr_resp" | jq -r .base.repo.full_name)
+BASE_BRANCH=$(echo "$pr_resp" | jq -r .base.ref)
 
 if [[ -z "$BASE_BRANCH" ]]; then
 	echo "Cannot get base branch information for PR #$PR_NUMBER!"
+  echo "API response: $pr_resp"
 	exit 1
 fi
 
+HEAD_REPO=$(echo "$pr_resp" | jq -r .head.repo.full_name)
+HEAD_BRANCH=$(echo "$pr_resp" | jq -r .head.ref)
+
 echo "Base branch for PR #$PR_NUMBER is $BASE_BRANCH"
+
+if [[ "$BASE_REPO" != "$HEAD_REPO" ]]; then
+	echo "PRs from forks are not supported at the moment."
+	exit 1
+fi
+
+git remote add upstream https://x-access-token:$GITHUB_TOKEN@github.com/$REPO_FULLNAME.git
+
+set -o xtrace
+git pull
+git checkout -b $HEAD_BRANCH
+git rebase $BASE_BRANCH
+git push --force upstream $HEAD_BRANCH
