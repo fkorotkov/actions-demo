6f13b5b96ad8b4dfc994dd45537a7d78c5baa8d0
Fedor Korotkov <fedor.korotkov@gmail.com>
2019-01-20 12:53:58 -0500

    Removed inlined email action

--- email/internal/email_template.go
+++
@@ -1,9 +0,0 @@
-package internal
-
-const DefaultSubjectTemplate = `
-{{.CheckSuite.App.Name}} check for {{.Repo.FullName}}#{{.CheckSuite.HeadBranch}} {{.CheckSuite.Status}}
-`
-
-const DefauleEmailMarkdownTemplate = `
-Check run [{{ .CheckRun.Name }}]({{ .CheckRun.HTMLURL }}) {{ .CheckRun.Status }} in {{ .Duration }}
-`
